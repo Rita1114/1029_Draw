6fab895dbb56baab1d808c7c9321304513038e3f
LAPTOP-K898G7OD\user <alley8332250@gmail.com>
2022-11-27 00:04:13 +0800

    1127-動畫切換成功，但人物會有慣性(??)*記得加遮罩擋傳送BUG

--- Assets/Scripts/Player/MoveAnimation.cs
+++ Assets/Scripts/Player/MoveAnimation.cs
@@ -5,67 +5,79 @@ using UnityEngine;
 
 public class MoveAnimation : MonoBehaviour
 {
-
     private Animator anim;
-    private string[] idleDirections ={"idle-leftup","idle-leftdown","idle-rightdown","idle-rightup"};
-    private string[] WalkDirections ={"walk-leftup","walk-leftdown","walk-rightdown","walk-rightup"};
+    private string[] idleDirections = { "idle-leftup", "idle-leftdown", "idle-rightdown", "idle-rightup" };
+    private string[] WalkDirections = { "walk-leftup", "walk-leftdown", "walk-rightdown", "walk-rightup" };
 
-     int lastDirection;
+    int lastDirection;
+
     // Start is called before the first frame update
     private void Awake()
     {
-        anim =GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 
-        float result1 =Vector2.SignedAngle(Vector2.up,Vector2.right);
-        Debug.Log("R1"+result1);
-        float result2 =Vector2.SignedAngle(Vector2.up,Vector2.left);
-        Debug.Log("R2"+result2);
-         float result3 =Vector2.SignedAngle(Vector2.up,Vector2.down);
-        Debug.Log("R3"+result3);
-       
+        float result1 = Vector2.SignedAngle(Vector2.up, Vector2.right);
+        Debug.Log("R1" + result1);
+        float result2 = Vector2.SignedAngle(Vector2.up, Vector2.left);
+        Debug.Log("R2" + result2);
+        float result3 = Vector2.SignedAngle(Vector2.up, Vector2.down);
+        Debug.Log("R3" + result3);
     }
 
     public void SetDirection(Vector2 _direction)
     {
-       string[] directionArray = null;
-       if(anim.GetInteger("Status")==0)
-       {
-           directionArray =idleDirections;
-       }
-       else
-       {
-           directionArray=WalkDirections;
-           lastDirection =DirectionToIndex(_direction);
-       }
-       anim.Play(directionArray[lastDirection]);
-        Debug.Log(_direction.magnitude);
+        string[] directionArray = null;
+        if (anim.GetBool("canwalk") == false)
+        {
+            directionArray = idleDirections;
+        }
+        else
+        {
+            directionArray = WalkDirections;
+            lastDirection = DirectionToIndex(_direction);
+        }
+
+        anim.Play(directionArray[lastDirection]);
+        
     }
 
     private int DirectionToIndex(Vector2 _direction)
     {
-        Vector2 noDir =_direction.normalized;
-        float step =360/4;
-        float offset =step/4 ;  
+        Vector2 noDir = _direction.normalized;
+        float step = 360 / 4;
+        float offset = step / 4;
 
-        float angle = Vector2.SignedAngle(Vector2.up,noDir);
+        float angle = Vector2.SignedAngle(Vector2.up, noDir);
 
         angle += offset;
         if (angle < 0)
         {
             angle += 360;
         }
-        float stepCount =angle / step;
+
+        float stepCount = angle / step;
         return Mathf.FloorToInt(stepCount);
+    }
+
+    public void WalkDirection(Vector2 walk_direction)
+    {
+        if (walk_direction.magnitude > 1)
+        {
+            anim.SetBool("canwalk", true);
+        }
+        else
+        {
+            anim.SetBool("canwalk", false);
+        }
+        Debug.Log(walk_direction.magnitude);
     }
 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
--- Assets/Scripts/Player/playerCtr1.cs
+++ Assets/Scripts/Player/playerCtr1.cs
@@ -1,61 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine.Utility;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 
 public class playerCtr1 : MonoBehaviour
 {
-
     public float Speed;
     public GameObject Bag;
     bool Isopen;
 
- 
+
     private Transform Door;
     private bool isDoor;
     private Transform playertransform;
     private Rigidbody2D rb;
     private Vector3 moveDir;
-    private float moveX,moveY;
-    [SerializeField] private float moveSpeed =5;
+    private float moveX, moveY,direction;
+    [SerializeField] private float moveSpeed = 5;
     private CharacterController characterBase;
-    
+
     public bool Getitem;
     public Animator anim;
     int canWalk;
     public Vector3 targetPoint;
+    private Vector2 MousePositionLast;
+    private Vector3 MouseWorldMoveDirection;
+
+    public float h, v;
+
 
     // Start is called before the first frame update
     void Start()
     {
-         canWalk=Animator.StringToHash("canwalk");
-        
     }
+
     private void Awake()
     {
-        characterBase=GetComponent<CharacterController>();
+        characterBase = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody2D>();
         targetPoint = transform.position;
         anim = GetComponent<Animator>();
-    } 
-
-        private void FixedUpdate()
-    {
-        transform.position =Vector3.MoveTowards(transform.position,targetPoint, moveSpeed*Time.deltaTime);
-        if(Input.GetMouseButtonDown(0))
-        {
-         anim.SetInteger("Status",1);
-         targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         targetPoint.z =0;
-       
-        }
-       
-            rb.velocity =new Vector2(targetPoint.x,targetPoint.y);
-
-            Vector2 direction =new Vector2(targetPoint.x,targetPoint.y);
-            FindObjectOfType<MoveAnimation>().SetDirection(direction);
     }
+
+    private void FixedUpdate()
+    {
+      
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPoint.z = 0;
+        }
+
+        rb.velocity = new Vector2(targetPoint.x, targetPoint.y);
+
+        Vector2 direction = new Vector2(targetPoint.x, targetPoint.y);
+        Vector2 Walkdirection = new Vector2(transform.position.x-targetPoint.x, transform.position.y-targetPoint.y).Abs();
+        FindObjectOfType<MoveAnimation>().WalkDirection(Walkdirection);
+        FindObjectOfType<MoveAnimation>().SetDirection(direction);
+        
+        
+        
+        
+    }
+
+    void Update()
+    {
+    }
+
     void OPBG()
     {
         if (Input.GetKeyDown(KeyCode.P))
@@ -64,22 +79,21 @@ public class playerCtr1 : MonoBehaviour
             Bag.SetActive(Isopen);
         }
     }
-      private  void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-     
-        if (other.name.ToLower().Contains("KEY"))//檢查名字
+        if (other.name.ToLower().Contains("KEY")) //檢查名字
         {
-            Destroy(other.gameObject);//刪除物件
+            Destroy(other.gameObject); //刪除物件
         }
     }
-    
-   
-public void function()
-{
-    if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime>1)
-    Debug.Log("not play");
-    else
-    Debug.Log("play");
-}
- 
+
+
+    public void function()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+            Debug.Log("not play");
+        else
+            Debug.Log("play");
+    }
 }
